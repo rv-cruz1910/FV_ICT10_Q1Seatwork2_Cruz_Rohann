b9aa3c9ed598236077fb34951f28caea9298bc82
rv-cruz1910 <cruz.rv@obmontessori.edu.ph>
2025-08-29 09:03:00 +0800

    Add files via upload

---
+++ SW.py
@@ -0,0 +1,22 @@
+
+
+store_name = "Volleyball Store"
+#this variable is a string
+owner_name = "Rohann Cruz"
+#this variable is a string
+year_established = 2025
+#this variable is an integer
+popular_item_price = 5,600
+#this variable is an integer
+has_delivery = True
+#this variable is a boolean
+product_names = ['Mikasa V300W','V5M5000', 'Mikasa V200W']
+#this variable is a list
+business_hours = "8 AM to 10 PM"
+#this variable is a string
+menu_prices = {'Mikasa V300W':'₱5,600', 'Molten V5M5000':'₱5,500', 'Mikasa V300W':'₱4,400','Mikasa BV550C':'₱5,500', 'Wilson OPTX AVP' : '₱3,000'}
+#this variable is a dictionary
+common_material = ['Leather', 'Glue', 'Sticky panels']
+#this variable is a list
+tax_rate = 0.5
+#this variable is a floating-point
